f709f5dc8256e35ed5ee37df318e802f6874385c
itc-s22004 <s22004@std.it-college.ac.jp>
2023-08-07 02:43:02 +0000

    s22004 index.js 変更

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-// import Image_Log from 'pic/----.jpg'
-
-const inter = Inter({ subsets: ['latin'] })
-
 const Home = () => {
   return (
-    <div>
-      {/* <figure>
-        <Image
-          src={Image_Log}  //--------------写真ない
-          alt=""
-          sizes="(min-width: 1152px) 1152px, 100vw"
-          priority
-          placeholder='blur'
-        />
-      </figure> */}
-      <div className='TalkGame'> 
-        <h3>お喋り,話題ゲーム</h3>
-        <h2>決まZゲーム</h2>
-      </div>
-      <div className='WordWolf'>
-        <h3>ゲーム</h3>
-        <h2>ワードウルフ  </h2>
-      </div>
-
-    </div>
+	  <p>s22004</p>
   )
 }
 
