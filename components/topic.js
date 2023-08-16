127cdbd54d6ebdba2a74a7d6f557f39edb6c8d0a
itc-s22004 <s22004@std.it-college.ac.jp>
2023-08-16 17:41:38 +0000

    Hint Buttonできた

--- components/topic.js
+++ components/topic.js
@@ -1,20 +1,38 @@
 import { useState } from "react"
+import Hint from "@/components/hint"
+import style from '@/styles/styles.module.css'
 
 const Topic = () => {
-  const topic_List = ["aaaa", "bbbb", "cccc", "dddd","eeeee","ffffff"]
+  const topic_List = ["START", "aaaa", "bbbb", "cccc", "dddd", "eeeee", "ffffff", "これであなた達は友達"]
   const [count, setCount] = useState(0)
+
   return (
-    <div>
+    <div className={style.sideBySideCenter}>
       <button
         type="button"
         onClick={() => {
           // var num = count + 1
-          setCount(count + 1);
-          // setCount(topic_List[count + 1])
-        }}>
+          setCount(count + 1)
+        }} className={style.topic_btn}>
         話 題
       </button>
-      Count: {topic_List[count]}
+      <div className={style.topic_pic}>
+        <div className={style.txt_center}>
+          {topic_List[count]}
+        </div>
+      </div>
+      <Hint
+        number="1"
+        topic_push_count={count}
+      />
+      <Hint
+        number="11"
+        topic_push_count={count}
+      />
+      {/* <Hint
+        number="3"
+        topic_push_count={count}
+      /> */}
     </div>
   )
 }
