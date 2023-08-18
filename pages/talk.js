acae245b2903f9a8161cd94f598092923ba2e28e
itc-s22019 <s22019@std.it-college.ac.jp>
2023-08-18 16:43:47 +0900

    ロゴ作成

--- pages/talk.js
+++ pages/talk.js
@@ -1,11 +1,12 @@
 import Layout from "@/components/layout"
 import Topic from "@/components/topic"
-
+import logo from "../styles/talk.png"
+import Image from 'next/image'
 
 const talk = () => {
     return (
         <Layout>
-            <h1>話題</h1>
+	    <Image className="logo" src={logo} />
             <Topic />
         </Layout>
     )
