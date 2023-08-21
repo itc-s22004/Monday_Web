d86180bd1f1ce84cfd771599dbcbf36bb3c472a7
itc-s22004 <s22004@std.it-college.ac.jp>
2023-08-21 01:22:00 +0000

    見た目　コード　完成

--- pages/talk.js
+++ pages/talk.js
@@ -1,12 +1,13 @@
 import Layout from "@/components/layout"
 import Topic from "@/components/topic"
-// import logo from "../styles/talk.png"
+import logo from "../styles/talk.png"
 import Image from 'next/image'
+import style from '@/styles/styles.module.css'
 
 const talk = () => {
     return (
         <Layout>
-	    {/* <Image className="logo" src={logo} /> */}
+	    <Image className={style.logo} src={logo} />
         <Topic />
         </Layout>
     )
